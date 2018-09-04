e2401550dcad7a7984524351326222b7fb0dec9f
t_maeshima_boot <t_maeshima@st.kobedenshi.ac.jp>
2018-09-04 21:02:05 +0900

    【Collision】 ・Cppファイル作成

--- 1年向けアクションゲーム/Collision.h
+++ 1年向けアクションゲーム/Collision.h
@@ -1,4 +1,7 @@
+#pragma once
+
 #include<d3dx9.h>
+#include<utility>
 
 enum eHitDirection
 {
@@ -9,51 +12,35 @@ enum eHitDirection
 	eNone,
 };
 
-static const eHitDirection RectCollision( const D3DXVECTOR2 & aPos , const D3DXVECTOR2 & aMoveVector , const D3DXVECTOR3 & aCenterPos
-                                        , const D3DXVECTOR2 & aTargetPos , const D3DXVECTOR3 & aTargetCenterPos )
+//線分構造体.
+struct tSegment
 {
-	//TODO:ゆくゆくは外部に逃がす.
-	const int length = 64;
-
-	const auto nextPos = aPos + aMoveVector;
-	const RECT rect = { aPos.x - aCenterPos.x , aPos.y - aCenterPos.y , aPos.x + ( length - aCenterPos.x ) , aPos.y + (length - aCenterPos.y) };
-	const RECT targetRect = { aTargetPos.x - aTargetCenterPos.x , aTargetPos.y - aTargetCenterPos.y , aTargetPos.x + (length - aTargetCenterPos.x) , aTargetPos.y + (length - aTargetCenterPos.y) };
-	const RECT nextRect = { nextPos.x - aCenterPos.x , nextPos.y - aCenterPos.y , nextPos.x + (length - aCenterPos.x) , nextPos.y + (length - aCenterPos.y) };
-
-	//上下判定.
-	if (targetRect.left < rect.right
-		&& targetRect.right > rect.left)
+	tSegment( const D3DXVECTOR2 & aPosition , const D3DXVECTOR2 & aVector )
 	{
-		//上判定.
-		if (targetRect.top < nextRect.bottom
-			&& targetRect.top > nextRect.top)
-		{
-			return eHitDirection::eUp;
-		}
-		//下判定.
-		else if (targetRect.bottom < nextRect.bottom
-			&& targetRect.bottom > nextRect.top)
-		{
-			return eHitDirection::eDown;
-		}
+		mPosition = aPosition;
+		mVector   = aVector;
 	}
-	//左右判定.
-	if (targetRect.top < rect.bottom
-		&& targetRect.bottom > rect.top)
-	{
-		//左判定.
-		if (targetRect.left < nextRect.right
-			&& targetRect.left > nextRect.left)
-		{
-			return eHitDirection::eLeft;
-		}
-		//右判定.
-		else if (targetRect.right < nextRect.right
-			&& targetRect.right > nextRect.left)
-		{
-			return eHitDirection::eRight;
-		}
-	}
+	D3DXVECTOR2 mPosition;
+	D3DXVECTOR2 mVector;
+};
 
-	return eHitDirection::eNone;
-}
+
+
+class Collision
+{
+public:
+	using SegmentsResult = std::pair<bool, D3DXVECTOR2>;
+// 線分の衝突
+	static const SegmentsResult Segments(
+		tSegment &seg1,          // 線分1
+		tSegment &seg2           // 線分2
+	);
+
+	// 2Dベクトルの外積
+	static const float D3DXVec2Cross(const D3DXVECTOR2 & v1, const D3DXVECTOR2 & v2);
+
+	static const eHitDirection Rect(const D3DXVECTOR2 & aPos, const D3DXVECTOR2 & aMoveVector, const D3DXVECTOR3 & aCenterPos
+		, const D3DXVECTOR2 & aTargetPos, const D3DXVECTOR3 & aTargetCenterPos);
+
+}; 
+
