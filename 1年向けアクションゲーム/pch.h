b4de54647079a466fbd3c4191d9ab282cd2dd3a3
t_maeshima@school <t_maeshima@st.kobedenshi.ac.jp>
2018-09-25 11:10:45 +0900

    【pch】 　・プリコンパイル済みヘッダー追加 　・d3dx9.hとwindows.hを全体でインクルードするように

---
+++ 1年向けアクションゲーム/pch.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include<string>
+#include<vector>
+#include<map>
+#include<functional>
+#include<algorithm>
+#include<cassert>
+#include<cmath>
+
+#include<Windows.h>
+#include<d3dx9.h>
